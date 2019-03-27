9f4f2de089c0e63641c77ed5f83e84560bee7237
cian001 <portisheadcian@gmail.com>
2019-03-27 10:26:22 +0000

    Add project files.

---
+++ ExcelWriteReader.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriteReader.Console
+{
+    /// <summary>
+    /// This script demonstrates reading data from an excel spreadsheet
+    /// using the ExcelWorkbook class
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
